ce48224baff0e18f0e969928bb188bed29bff287
Cruz <andressmaldonado1@gmail.com>
2021-01-11 09:40:57 -0400

    Empiezo a dibujar.

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,1 +1,15 @@
-     
+var d=document.getElementById("dibujito");
+var lienzo=d.getContext("2d");
+dibujarLinea("black",0,0,100,200);
+function dibujarLinea(color,xInicial,yInicial,xFianl,yFianl)
+{
+    lienzo.beginPath();
+    lienzo.strokeStyle=color;
+    lienzo.moveTo(xInicial,yInicial);
+    lienzo.lineTo(xFianl,yFianl);
+    lienzo.stroke();
+    lienzo.closePath();
+
+
+
+}

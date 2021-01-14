cca257c277756ea222489a50808f15cddec22dc1
Cruz <andressmaldonado1@gmail.com>
2021-01-14 12:24:24 -0400

    Este programa puede dibujar lineas con teclado y pintar con el mouse, tambien puede cambiar el color de la fuente y su grosor. PDTA: Debo modificar: 1-) Cambiar el color de la fuente con solo una funcion. 2-)Cambiar el grosor de la fuente con solo una funcion. 3.)Cambiar la variable fuente a variables normales , si no la utilizo   para arreglar los problemas 1 y 2;

--- teclas/eventos.js
+++ teclas/eventos.js
@@ -6,18 +6,42 @@ como botton*/
 //keyup es para detectar teclas... y keydown??
 
 var teclas={LEFT:37, DOWN:38, RIGHT:39, UP:40};
-document.addEventListener("keyup",dibujarTeclado);
 var cuadro=document.getElementById("area_dibujo");
 var papel =cuadro.getContext("2d");
+var colorfuente="blue";
+var grosor=3;
+
+var fuente=
+{
+   verde :document.getElementById("colorVerde"),
+   azul :document.getElementById("colorAzul"),
+   negro : document.getElementById("colorNegro"),
+   rojo :document.getElementById("colorRojo"),
+   blanco : document.getElementById("colorBlanco"),
+   grueso : document.getElementById("grosor1"),
+   delgado : document.getElementById("grosor2")
+    
+}
+fuente.verde.addEventListener("click", cambiarColorVerde );
+fuente.azul.addEventListener("click", cambiarColorAzul );
+fuente.negro.addEventListener("click", cambiarColorNegro );
+fuente.rojo.addEventListener("click", cambiarColorRojo );
+fuente.blanco.addEventListener("click", cambiarColorBlanco );
+fuente.grueso.addEventListener("click", cambiarGrosorMas );
+fuente.delgado.addEventListener("click", cambiarGrosorMenos );
+document.addEventListener("keyup",dibujarTeclado);
+
+
 var x=150,y=150;
+var click=false;
 dibujarLinea("red", x-1, y-1, x+1, y+1, papel);
-console.log(document);
-console.log(window);
-console.log(area_dibujo);
-    document-addEventListener("mouseup",dibujarMouse);
+    document.addEventListener("mousedown",dibujarMouse);
+    document.addEventListener("mouseup",soltarMouse);
+    document.addEventListener("mousemove",pintarMouse);
+
 function dibujarTeclado(evento)
 {
-    var colorcito="blue";
+    var colorcito=colorfuente;
     var movimiento=10;
 switch(evento.keyCode)
 {
@@ -42,7 +66,7 @@ case teclas.RIGHT:
 function dibujarLinea(color,xInicial,yInicial,xFianl,yFianl,lienzo)
 {
     lienzo.beginPath();
-    lienzo.lineWidth=3;
+    lienzo.lineWidth=grosor;
     lienzo.strokeStyle=color;
     lienzo.moveTo(xInicial,yInicial);
     lienzo.lineTo(xFianl,yFianl);
@@ -52,7 +76,58 @@ function dibujarLinea(color,xInicial,yInicial,xFianl,yFianl,lienzo)
 
 
 }
-function dibujarMouse(evento)
+function dibujarMouse(evento2)
 {
-console.log(evento);
+var colorcito=colorfuente;
+ click=true;
+x=(evento2.layerX+1);
+y=(evento2.layerY+1);  
+    dibujarLinea(colorcito,x,y  ,evento2.layerX-1,evento2.layerY-1,papel);
+    x=evento2.layerX;
+    y=evento2.layerY;  
+    
+}
+function soltarMouse(evento)
+{
+     click=false;
+
+}
+function pintarMouse(evento)
+{
+var colorcito=colorfuente;
+x=(evento.layerX+1);
+y=(evento.layerY+1);  
+    if (click==true){
+    dibujarLinea(colorcito,x,y  ,evento.layerX-1,evento.layerY-1,papel);
+    x=evento.layerX;
+    y=evento.layerY;  
+    }    
+}
+function cambiarColorVerde()
+{
+    colorfuente="green";
+}
+function cambiarColorRojo()
+{
+    colorfuente="red";
+}
+function cambiarColorBlanco()
+{
+    colorfuente="white";
+}
+function cambiarColorNegro()
+{
+    colorfuente="black";
+}
+function cambiarColorAzul()
+{
+    colorfuente="blue";
+}
+function cambiarGrosorMas()
+{
+grosor++;
+}
+function cambiarGrosorMenos()
+{
+grosor--;
 }
